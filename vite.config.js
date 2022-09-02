4489d195c3dd628bba24a695f2ddf98ab5eb4898
VladYankov <vladankov996@gmail.com>
2022-09-02 14:23:15 +0300

    change config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: "/test-task-todo-vue3/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
