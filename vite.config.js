ee1a52c5bea140db2854e602a184ca90a5d7ae86
VladYankov <vladankov996@gmail.com>
2022-09-02 14:08:11 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: "/test-task-todo-vue3/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
