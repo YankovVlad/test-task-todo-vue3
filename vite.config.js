d9b7a66af55484173a5ad44e0c05f410b8171e9f
VladYankov <vladankov996@gmail.com>
2022-09-02 15:44:35 +0300

    change config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
